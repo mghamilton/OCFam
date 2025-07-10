4ab6ea7cec48ef15dc8906bd71057d74b72af49d
Hamilton <m.hamilton@cgiar.org>
2025-07-10 15:08:38 +1000

    OCFamPrep functioning

--- R/OCFamPrep.R
+++ R/OCFamPrep.R
@@ -40,13 +40,18 @@ ped[ped[,"DAM"] == "","DAM"] <- 0
 #'  \item{'AVAIL_BROOD' is TRUE if the individual is candidate parent of next age class (logical).}
 #'  }
 #'
+#'#' @param age_class_names is an data frame specifying names for age classes (class in parentheses):
+#' \itemize{
+#'  \item{'AGE_CLASS_NAME' is a name for the age class (character).}
+#'  \item{'BORN' integer indicating age class.  May be the year of birth if one age class per year or an integer indicating the sequence of age classes (integer).}
+#'  }
 #'
 #' @param gene_flow_vector is a applicable to overlapping generations (if NA discrete generations is assumed).  It is vector representing parental contributions by age class to the next age class. For example, gene_flow_vector = c(0.2, 0.8, 0, 0) - oldest age class to youngest age class.
 
 #Required packages: AGHmatrix, dplyr, ggplot2
 
 
-OCFam_fam_K_matrix <- function(family_dat) {
+OCFam_fam_K_matrix <- function(family_ped) {
   #data
   # class: data.frame
   # fields:
@@ -57,26 +62,26 @@ OCFam_fam_K_matrix <- function(family_dat) {
   #   FAM_DAM
 
   #sires
-  sires <- matrix(unique(family_dat$SIRE), ncol = 1)
+  sires <- matrix(unique(family_ped$SIRE), ncol = 1)
   colnames(sires)[1] <- "SIRE"
-  sires <- merge(sires, family_dat[,c("SIRE","FAM_SIRE")], by = "SIRE", all.x = TRUE)
+  sires <- merge(sires, family_ped[,c("SIRE","FAM_SIRE")], by = "SIRE", all.x = TRUE)
   colnames(sires) <- c("Indiv_id","FAM")
-  sires <- merge(sires, family_dat[,c("FAM","SIRE","DAM")], by = "FAM", all.x = TRUE)
+  sires <- merge(sires, family_ped[,c("FAM","SIRE","DAM")], by = "FAM", all.x = TRUE)
   sires <- subset(sires, select=-c(FAM)) #remove FAM column
 
   #dams
-  dams <- matrix(unique(family_dat$DAM), ncol = 1)
+  dams <- matrix(unique(family_ped$DAM), ncol = 1)
   colnames(dams)[1] <- "DAM"
-  dams <- merge(dams, family_dat[,c("DAM","FAM_DAM")], by = "DAM", all.x = TRUE)
+  dams <- merge(dams, family_ped[,c("DAM","FAM_DAM")], by = "DAM", all.x = TRUE)
   colnames(dams) <- c("Indiv_id","FAM")
-  dams <- merge(dams, family_dat[,c("FAM","SIRE","DAM")], by = "FAM", all.x = TRUE)
+  dams <- merge(dams, family_ped[,c("FAM","SIRE","DAM")], by = "FAM", all.x = TRUE)
   dams <- subset(dams, select=-c(FAM)) #remove FAM column
 
   parents <- rbind(sires,dams)
   parents <- unique(parents)
   parents <- parents[order(parents[,"Indiv_id"], decreasing = FALSE), ]
 
-  families            <- unique(family_dat)
+  families            <- unique(family_ped)
   tmp1 <- families
   tmp2 <- families
   tmp1$FAM <- paste0("1_", tmp1$FAM) #needs a unique id
@@ -105,32 +110,34 @@ OCFam_fam_K_matrix <- function(family_dat) {
 
 
 
-OCFamPrep <- function(ped, gene_flow_vector = NA) {
+OCFamPrep <- function(ped, age_class_names = NULL, gene_flow_vector = NULL) {
   library(ggplot2)
+  if(length(gene_flow_vector) == 1) {gene_flow_vector <- NULL}
+  if(length(age_class_names) == 1) {age_class_names <- NULL}
   #library(dplyr)
   #library(AGHmatrix)
 
-  #  family_dat <- family_dat[,c("FAM", "SIRE", "DAM", "FAM_SIRE", "FAM_DAM", "SPAWN_ROUND", "SPECIES", "FAM_NEST")]
+  #  family_ped <- family_ped[,c("FAM", "SIRE", "DAM", "FAM_SIRE", "FAM_DAM", "SPAWN_ROUND", "SPECIES", "FAM_NEST")]
 
-  #create family_dat
-  family_dat <- unique(ped[,c("FAM", "SIRE", "DAM", 'BORN')])
+  #create family_ped
+  family_ped <- unique(ped[,c("FAM", "SIRE", "DAM", 'BORN')])
   sires <- ped[,c('INDIV', 'FAM', "EBV")]
   colnames(sires) <- c("SIRE", "FAM_SIRE", "EBV_SIRE")
-  family_dat <- dplyr::left_join(family_dat, sires, by = "SIRE")
+  family_ped <- dplyr::left_join(family_ped, sires, by = "SIRE")
   rm(sires)
   dams <- ped[,c('INDIV', 'FAM', "EBV")]
   colnames(dams) <- c("DAM", "FAM_DAM", "EBV_DAM")
-  family_dat <- dplyr::left_join(family_dat, dams, by = "DAM")
+  family_ped <- dplyr::left_join(family_ped, dams, by = "DAM")
   rm(dams)
 
-  family_dat$EBV_FAM <- (family_dat$EBV_SIRE + family_dat$EBV_DAM)/2
+  family_ped$EBV_FAM <- (family_ped$EBV_SIRE + family_ped$EBV_DAM)/2
 
-  family_dat[family_dat$FAM == "", "FAM"] <- NA
-  family_dat <- family_dat[!is.na(family_dat$FAM),]
+  family_ped[family_ped$FAM == "" | is.na(family_ped$FAM), "FAM"] <- NA
+  family_ped <- family_ped[!is.na(family_ped$FAM),]
 
-  ped <- dplyr::left_join(ped, family_dat, by = c("FAM", "SIRE", "DAM", 'BORN'))
+  ped <- dplyr::left_join(ped, family_ped, by = c("FAM", "SIRE", "DAM", 'BORN'))
 
-  fam_K_matrix <- OCFam_fam_K_matrix(family_dat)
+  fam_K_matrix <- OCFam_fam_K_matrix(family_ped)
 
   family_inbreeding <- data.frame(FAM = names(fam_K_matrix$family_inbreeding),
                                   F    = fam_K_matrix$family_inbreeding)
@@ -143,28 +150,75 @@ OCFamPrep <- function(ped, gene_flow_vector = NA) {
   #Generate plots
   ####################################################################################
 
-  age_classs <- unique(family_dat$BORN)
+  age_classs <- unique(family_ped$BORN)
 
-  age_class_K <- data.frame(BORN = NA, VARIABLE = NA, MEAN = NA)[-1,]
-  age_class_F <- age_class_K
+  age_class_K_mat <- matrix(data = NA, nrow = length(age_classs), ncol = length(age_classs))
+  rownames(age_class_K_mat) <- age_classs
+  colnames(age_class_K_mat) <- age_classs
+
   #get means
-  for(sr in age_classs) {
-    fams <- family_dat[family_dat$BORN == sr,"FAM"]
 
-    tmp_mean <- mean(as.matrix(fam_K_matrix[fams,fams]))
-    tmp <- data.frame(BORN = sr, VARIABLE = "Coancestry (k)", MEAN = tmp_mean)
-    age_class_K <- rbind(age_class_K, tmp)
-
+  age_class_F <- data.frame(BORN = NA, VARIABLE = NA, MEAN = NA)[-1,]
+  for(ac in age_classs) {
+    fams <- family_ped[family_ped$BORN == as.character(ac),"FAM"]
     tmp_F <- mean(family_inbreeding[family_inbreeding$FAM %in% fams, "F"])
-    tmp <- data.frame(BORN = sr, VARIABLE = "Inbreeding (F)", MEAN = tmp_F)
+    tmp <- data.frame(BORN = ac, VARIABLE = "Inbreeding (F)", MEAN = tmp_F)
     age_class_F <- rbind(age_class_F, tmp)
+  }
+
+  #coancestry matrix
+  for(ac1 in age_classs) {
+    for(ac2 in age_classs) {
+
+      if(ac1 >= ac2){
+        fams1 <- family_ped[family_ped$BORN == as.character(ac1),"FAM"]
+        fams2 <- family_ped[family_ped$BORN == as.character(ac2),"FAM"]
+
+        tmp_mean <- mean(as.matrix(fam_K_matrix[fams1,fams2]))
+        age_class_K_mat[as.character(ac1), as.character(ac2)] <-  tmp_mean
+      }
+    }
+  }
+  age_class_K_mat[upper.tri(age_class_K_mat)] <- age_class_K_mat[lower.tri(age_class_K_mat)]
+  age_class_K <- data.frame(BORN = rownames(age_class_K_mat), VARIABLE = "Coancestry (k)", MEAN = diag(age_class_K_mat))
+
+  if(is.null(gene_flow_vector)) {
+    age_class_means <- rbind(age_class_F, age_class_K)
 
   }
 
-  age_class_means <- rbind(age_class_K, age_class_F)
+    age_class_K_overlap <-  data.frame(BORN = age_classs, VARIABLE = "Coancestry (overlapping; k)", MEAN = NA)
 
-  if(is.na(gene_flow_vector)) {
-    p <- ggplot(age_class_means, aes(x=BORN, y=MEAN, group=VARIABLE)) +
+  if(!is.null(gene_flow_vector)) {
+    for(ac in age_classs[length(gene_flow_vector):length(age_classs)]) {
+      age_class_K_overlap[age_class_K_overlap$BORN == ac,"MEAN"] <-  t(gene_flow_vector) %*%
+        age_class_K_mat[as.character((ac-length(gene_flow_vector)+1):ac),
+                        as.character((ac-length(gene_flow_vector)+1):ac)] %*%
+        gene_flow_vector
+    }
+    age_class_means <- rbind(age_class_F, age_class_K, age_class_K_overlap)
+  }
+
+    age_class_means$BORN <-  as.numeric(age_class_means$BORN)
+
+   if(is.null(age_class_names)) {
+  p <- ggplot(age_class_means, aes(x=BORN, y=MEAN, group=VARIABLE))
+   } else {
+     age_class_means <- dplyr::left_join(age_class_means, age_class_names, by = "BORN")
+     ped <- dplyr::left_join(ped, age_class_names, by = "BORN")
+     family_ped <- dplyr::left_join(family_ped, age_class_names, by = "BORN")
+
+     # Ensure AGE_CLASS_NAME is ordered by BORN
+     age_class_means <- age_class_means[order(age_class_means$BORN), ]
+
+     # Reassign AGE_CLASS_NAME as a factor, now ordered by BORN
+     age_class_means$AGE_CLASS_NAME <- factor(age_class_means$AGE_CLASS_NAME,
+                                              levels = unique(age_class_means$AGE_CLASS_NAME))
+
+     p <- ggplot(age_class_means, aes(x=AGE_CLASS_NAME, y=MEAN, group=VARIABLE))
+
+   }
+    p <- p +
       geom_line(aes(color=VARIABLE))+
       geom_point(aes(color=VARIABLE)) +
       #  theme_bw()  +
@@ -172,18 +226,24 @@ OCFamPrep <- function(ped, gene_flow_vector = NA) {
       theme( # remove the vertical grid lines
         panel.grid.major.x = element_blank() ,
         # explicitly set the horizontal lines (or they will disappear too)
-        panel.grid.major.y = element_line(linewidth=.1, color="grey70" )
+        panel.grid.major.y = element_line(linewidth=.1, color="grey70" ),
+        axis.text.x = element_text(angle = 90, vjust = 0.5)
       )
-    p
-  }
-  age_class_means <- data.frame(BORN = unique(age_class_means$BORN),
-                                COANCESTRY = age_class_means[age_class_means$VARIABLE == "Coancestry (k)","MEAN"],
-                                INBREEDING = age_class_means[age_class_means$VARIABLE == "Inbreeding (F)","MEAN"])
+  p
+
+  age_class_means <- data.frame(BORN = age_classs,
+                                INBREEDING = age_class_F$MEAN,
+                                COANCESTRY = age_class_K$MEAN,
+                                OVERLAPPING_COANCESTRY = age_class_K_overlap$MEAN)
 
   return(list(plot = p,
               fam_K_matrix = fam_K_matrix,
+              age_class_K_mat = age_class_K_mat,
               family_inbreeding = family_inbreeding,
-              age_class_means = age_class_means))
+              age_class_means = age_class_means,
+              ped = ped,
+              family_ped = family_ped,
+              gene_flow_vector = gene_flow_vector))
 }
 
 
